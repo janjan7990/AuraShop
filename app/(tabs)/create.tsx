76a250308da32f3d8162e3dfd8649e5aeb2832dd
janjan7990 <janjanthanaporn6@gmail.com>
2025-03-14 11:47:21 +0700

    Update

---
+++ app/(tabs)/create.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaFrameContext } from 'react-native-safe-area-context'
+
+export default function TabCreate() {
+  return (
+    <SafeAreaView style={{flex: 1}} >
+        <View style={{flex:1, justifyContent: "center",alignItems: "center"}}>
+            <Text>setting</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaFrameContext } from 'react-native-safe-area-context'
+
+export default function TabProfile() {
+  return (
+    <SafeAreaView style={{flex: 1}} >
+        <View style={{flex:1, justifyContent: "center",alignItems: "center"}}>
+            <Text>setting</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
